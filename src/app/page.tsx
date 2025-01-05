7e9cea4c993ea1d817bc61f52617687d18e6b1eb
Muhammad Kaif Nazeer <122388541+MuhammadKaifNazeer@users.noreply.github.com>
2025-01-05 12:21:10 +0000

    update: homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,77 +6,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
   return (
-    <div className="max-w-3xl mx-auto p-4 space-y-4 relative">
-      <div className="pointer-events-none absolute -right-64 -top-48" aria-hidden="true">
-        <svg xmlns="http://www.w3.org/2000/svg" width="856" height="745" fill="none">
-          <g filter="url(#ill-a)">
-            <path
-              fill="url(#ill-b)"
-              fillRule="evenodd"
-              d="m56 88 344 212-166 188L56 88Z"
-              clipRule="evenodd"
-            />
-          </g>
-          <g filter="url(#ill-c)">
-            <path
-              fill="url(#ill-d)"
-              fillRule="evenodd"
-              d="m424 257 344 212-166 188-178-400Z"
-              clipRule="evenodd"
-            />
-          </g>
-          <defs>
-            <linearGradient
-              id="ill-b"
-              x1="210.5"
-              x2="210.5"
-              y1="88"
-              y2="467"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#fff" stopOpacity="0.64" />
-              <stop offset="1" stopColor="#fff" stopOpacity="0" />
-            </linearGradient>
-            <linearGradient
-              id="ill-d"
-              x1="578.5"
-              x2="578.5"
-              y1="257"
-              y2="636"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#fff" stopOpacity="0.64" />
-              <stop offset="1" stopColor="#fff" stopOpacity="0" />
-            </linearGradient>
-            <filter
-              id="ill-a"
-              width="520"
-              height="576"
-              x="-32"
-              y="0"
-              colorInterpolationFilters="sRGB"
-              filterUnits="userSpaceOnUse"
-            >
-              <feFlood floodOpacity="0" result="BackgroundImageFix" />
-              <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-              <feGaussianBlur result="effect1_foregroundBlur_244_5" stdDeviation="44" />
-            </filter>
-            <filter
-              id="ill-c"
-              width="520"
-              height="576"
-              x="336"
-              y="169"
-              colorInterpolationFilters="sRGB"
-              filterUnits="userSpaceOnUse"
-            >
-              <feFlood floodOpacity="0" result="BackgroundImageFix" />
-              <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-              <feGaussianBlur result="effect1_foregroundBlur_244_5" stdDeviation="44" />
-            </filter>
-          </defs>
-        </svg>
-      </div>
+    <div className="max-w-3xl mx-auto p-4 space-y-4">
       <HeroSection />
       <Tabs defaultValue="tab-1" className="mt-6">
         <TabsList className="h-auto w-full rounded-none border-b border-border bg-transparent p-0">
